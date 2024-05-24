267c812fe2d8eba6b68b4877f78700166f34e877
dkowalczyk <d.kowalczyk@dituel.pl>
2024-05-24 16:50:56 +0200

    board render, clickHandle

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import GameBoard from "./components/GameBoard";
+import Player from "./components/Player";
+
+function App() {
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="Player1" symbol="X" />
+          <Player initialName="Player2" symbol="O" />
+        </ol>
+        <GameBoard />
+      </div>
+      LOG
+    </main>
+  );
+}
+
+export default App;
