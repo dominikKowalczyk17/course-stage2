f9b637af09c4a6ed97904e39df407d132d665ad0
dkowalczyk <kowalczyk.dominik17@gmail.com>
2024-05-28 20:37:41 +0200

    lifting useState, deriving state

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,49 @@
 import GameBoard from "./components/GameBoard";
+import Log from "./components/Log";
 import Player from "./components/Player";
+import { useState } from "react";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+  const [gameTurns, setGameTurns] = useState([]);
+
+  const handleSelectSquare = (rowIndex, colIndex) => {
+    setActivePlayer((currentActivePlayer) =>
+      currentActivePlayer === "X" ? "O" : "X"
+    );
+    setGameTurns((prevTurns) => {
+      let currentPlayer = "X";
+
+      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+        currentPlayer = "O";
+      }
+
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+
+      return updatedTurns;
+    });
+  };
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player1" symbol="X" />
-          <Player initialName="Player2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        <GameBoard />
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
-      LOG
+      <Log />
     </main>
   );
 }
